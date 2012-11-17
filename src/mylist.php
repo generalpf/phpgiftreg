f346967184885c842a61157fac7593de6b364464
Ryan Walberg <generalpf@gmail.com>
2012-11-17 04:06:40 +0000

    converted My Items

--- src/mylist.php
+++ src/mylist.php
@@ -26,37 +26,6 @@ else {
 	$userid = $_SESSION["userid"];
 }
 
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\r\n";
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-<title>Gift Registry - Shopping List</title>
-<script language="JavaScript">
-	function printPage() {
-		window.print();
-	}
-</script>
-<link href="styles.css" type="text/css" rel="stylesheet" />
-</head>
-<body>
-<p class="pagetitle">Gift Registry - Wish List for <?php echo $_SESSION["fullname"]; ?></p>
-<?php
-if ($OPT["show_helptext"]) {
-	?>
-	<p>
-		<div class="helptext">
-			<ul>
-				<li>You can click the column headers to sort by that attribute.</li>
-				<li>Once you've bought or decided not to buy an item, remember to return to the recipient's gift lists and mark it accordingly.</li>
-				<li><strong>Please login to the Gift Registry site to get the most recent version of this list.</strong></li>
-				<li>For better printing results, please change your print orientation to "Landscape" mode.</li>
-			</ul>
-		</div>
-	</p>
-	<?php
-}
-
 if (empty($_GET["sort"]))
 	$sort = "source";
 else
@@ -89,64 +58,29 @@ $query = "SELECT description, source, price, i.comment, i.quantity, i.quantity *
 			"LEFT OUTER JOIN {$OPT["table_prefix"]}categories c ON c.categoryid = i.category " .
 			"WHERE u.userid = " . $_SESSION["userid"] . " " .
 			"ORDER BY $sortby";
-$shoplist = mysql_query($query) or die("Could not query $query: " . mysql_error());
+$rs = mysql_query($query) or die("Could not query $query: " . mysql_error());
+$shoplist = array();
+$totalprice = 0;
+while ($row = mysql_fetch_array($rs, MYSQL_ASSOC)) {
+	$totalprice += $row["total"];
+	if ($row["quantity"] == 1)
+		$row["price"] = formatPrice($row["price"]);
+	else
+		$row["price"] = $row["quantity"] . " @ " . formatPrice($row["price"]) . " = " . formatPrice($row["total"]);
+	$shoplist[] = $row;
+}
+$itemcount = mysql_num_rows($rs);
+mysql_free_result($rs);
+
+define('SMARTY_DIR',str_replace("\\","/",getcwd()).'/includes/Smarty-3.1.12/libs/');
+require_once(SMARTY_DIR . 'Smarty.class.php');
+$smarty = new Smarty();
+$smarty->assign('shoplist', $shoplist);
+$smarty->assign('totalprice', formatPrice($totalprice));
+$smarty->assign('itemcount', $itemcount);
+$smarty->assign('userid', $userid);
+$smarty->assign('isadmin', $_SESSION["admin"]);
+$smarty->assign('opt', $OPT);
+$smarty->display('mylist.tpl');
 ?>
-<p>
-	<table class="partbox" width="100%" cellspacing="0">
-		<!--<tr class="partboxtitle">
-			<td colspan="5" align="center">Wish List for <?php echo $_SESSION["fullname"]; ?></td>
-		</tr>-->
-		<tr>
-			<th class="colheader"><a href="mylist.php?sort=ranking">Ranking</a></th>
-			<th class="colheader"><a href="mylist.php?sort=source">Source</a></th>
-			<th class="colheader"><a href="mylist.php?sort=description">Description</a></th>
-			<th class="colheader"><a href="mylist.php?sort=category">Category</a></th>
-			<th class="rcolheader"><a href="mylist.php?sort=price">Price</a></th>
-		</tr>
-		<?php
-		$i = 0;
-		$totalprice = 0;
-		while ($row = mysql_fetch_array($shoplist,MYSQL_ASSOC)) {
-			$totalprice += $row["total"];
-			?>
-			<tr class="<?php echo (!($i++ % 2)) ? "evenrow" : "oddrow" ?>">
-				<td nowrap><?php echo $row["rendered"]; ?></td>
-				<td><?php echo $row["source"]; ?></td>
-				<td><?php echo $row["description"]; ?></td>
-				<td nowrap><?php echo $row["category"]; ?></td>
-				<td align="right">
-					<?php
-					if ($row["quantity"] == 1)
-						echo formatPrice($row["price"]);
-					else
-						echo $row["quantity"] . "&nbsp;@&nbsp;" . formatPrice($row["price"]) . "&nbsp;=&nbsp;" . formatPrice($row["total"]);
-					?>
-				</td>
-			</tr>
-			<?php
-			if ($row["comment"] != "") {
-				?>
-				<tr class="<?php echo (($i % 2) ? "evenrow" : "oddrow"); ?>">
-					<td>&nbsp;</td>
-					<td colspan="3">
-						<i><?php echo str_replace("\r\n","<br />",$row["comment"]); ?></i>
-					</td>
-					<td>&nbsp;</td>
-				</tr>
-				<?php
-			}
-		}
-		?>
-	</table>
-</p>
-<p align="right">
-	<?php echo mysql_num_rows($shoplist) . " item(s), {$OPT["currency_symbol"]}$totalprice total."; ?>
-</p>
-<?php
-mysql_free_result($shoplist);
-?>
-<p>
-	<a onClick="printPage()" href="#">Send to printer</a>&nbsp;/&nbsp;<a href="index.php">Back to Main</a>
-</p>
-</body>
-</html>
+
